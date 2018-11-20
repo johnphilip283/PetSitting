a75fcb66f86678a602cbc674542e46c7c10900dc
John Philip <johnphilip283@gmail.com>
2018-11-20 15:52:35 -0500

    added server code

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mysql = require("mysql");
+
+const GET_ALL_PETS_QUERY = "SELECT * FROM pet;";
+const ADD_PET_QUERY = "INSERT INTO pet (name, age, description, owner_id, species_id) VALUES (req.body.name, req.body.age, " +
+    "req.body.description, req.body.owner_id, req.body.species_id)";
+
+const connection = mysql.createConnection({
+   host: 'localhost',
+   user: 'root',
+   password: '',
+   database: 'petsitting'
+});
+
+connection.connect(err => {
+  if (err) {
+      return err;
+  }
+});
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+
+});
+
+app.post('/pets/add', function(req, res) {
+    console.log("here")
+    connection.query(ADD_PET_QUERY, (err, results) => {
+        if (err) {
+            return res.send(err)
+        } else {
+            console.log(results);
+            return res.json({
+                data: results
+            })
+        }
+    })
+
+
+});
+
+app.get("/pets", (req, res) => {
+    console.log("at /pets hurr")
+    connection.query(GET_ALL_PETS_QUERY, (err, results) => {
+        if (err) {
+            return res.send(err)
+        } else {
+            return res.json({
+                data: results
+            })
+        }
+    })
+});
+
+
+
+app.listen(4000, () => {
+    console.log("potato on port 4000");
+});
--- src/components/home/Homepage.js
+++ src/components/home/Homepage.js
@@ -6,12 +6,51 @@ export default class Homepage extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            pets: []
+        };
     }
 
+    componentDidMount() {
+        this.getPets();
+    }
+
+    getPets = _ => {
+        fetch('http://localhost:4000/pets')
+            .then(response => response.json())
+            .then(response => this.setState({pets: response.data}))
+            .catch(err => console.error(err))
+    };
+
+    addPet = () => {
+
+        fetch('https://localhost:4000/pets/add/', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            credentials: "same-origin",
+            body: JSON.stringify({
+                name: 'Tuppy',
+                age: 10,
+                description: 'Tuppy Wuppy is a fluffy.',
+                owner_id: 2,
+                species_id: 6
+            })
+        }).then(response => response.json())
+    };
+
+    renderPet = ({pet_id, name, description}) => <div key={pet_id}> {name} <br/> {description} </div>
+
+
     render() {
+        const {pets} = this.state;
         return (
             <div className={'home'}>
-                <p>I want a bunny</p>
+                {pets.map(this.renderPet)}
+                <button onClick={() => this.addPet()}>Add Chinny </button>
             </div>
 
         );
